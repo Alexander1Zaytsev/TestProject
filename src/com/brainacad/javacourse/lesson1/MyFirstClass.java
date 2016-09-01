ff0b760c54705fb2620267d16801e00b719975a8
Alexander1Zaytsev <alexander.zaytsev1@gmail.com>
2016-09-01 18:03:59 +0100

    Initial commit

---
+++ src/com/brainacad/javacourse/lesson1/MyFirstClass.java
@@ -0,0 +1,10 @@
+package com.brainacad.javacourse.lesson1;
+
+/**
+ * Created by User on 30/08/2016.
+ */
+public class MyFirstClass {
+    public static void main(String[] arg){
+        System.out.println("Hello hello brain academy!");
+    }
+}
