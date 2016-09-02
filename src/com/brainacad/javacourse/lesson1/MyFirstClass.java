13d770d4b07bdb52f78e51851c47aae6a59bdd83
Alexander1Zaytsev <Alexander.Zaytsev1@gmail.com>
2016-09-02 12:57:34 +0300

    seconcommit

--- src/com/brainacad/javacourse/lesson1/MyFirstClass.java
+++ src/com/brainacad/javacourse/lesson1/MyFirstClass.java
@@ -1,10 +1,14 @@
 package com.brainacad.javacourse.lesson1;
-
+import java.util.Scanner;
 /**
  * Created by User on 30/08/2016.
  */
 public class MyFirstClass {
     public static void main(String[] arg){
         System.out.println("Hello hello brain hjfjkhllsdv academy!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input smth");
+        String str = sc.nextLine();
+        System.out.println(str);
     }
 }
